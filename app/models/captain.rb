8c9e282321faacf74dbc00ef74bcbfd9436f4cad
Matteo Muscas <thatman.j@gmail.com>
2019-06-26 13:43:38 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,23 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: { name: 'Catamaran' })
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: { name: 'Sailboat' }).uniq
+  end
+
+  def self.motorboats
+    includes(boats: :classifications).where(classifications: { name: 'Motorboat' }).uniq
+  end
+
+  def self.talented_seafarers
+    sailors & motorboats
+  end
+
+  def self.non_sailors
+    all - sailors
+  end
 end
